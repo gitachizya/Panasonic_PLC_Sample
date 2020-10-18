bc1bd9c589210f265eb347caa854a96bd8dbc7fd
gitachizya <gitachizya@google.com>
2020-10-18 16:24:53 +0000

    Updates

---
+++ precache-manifest.029a81801779f2c3b52109601da38e96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160c2d7c207023d029ca3797b3ee81aa",
+    "url": "/Panasonic_PLC_Sample/index.html"
+  },
+  {
+    "revision": "f57376f2789f2735382a",
+    "url": "/Panasonic_PLC_Sample/static/css/main.b37a7b5f.chunk.css"
+  },
+  {
+    "revision": "8bf93023440ba1a91be2",
+    "url": "/Panasonic_PLC_Sample/static/js/2.8b44c45b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Panasonic_PLC_Sample/static/js/2.8b44c45b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57376f2789f2735382a",
+    "url": "/Panasonic_PLC_Sample/static/js/main.2b745f98.chunk.js"
+  },
+  {
+    "revision": "1e0eb964410d6ab2b2af",
+    "url": "/Panasonic_PLC_Sample/static/js/runtime-main.f5193442.js"
+  }
+]);
